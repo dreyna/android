d23784c20b3daf0b594e419e0cbb5cdd3d088688
David Reyna <reyna_b@upeu.edu.pe>
2019-08-01 17:46:00 -0500

    wwew

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn1 = findViewById(R.id.btnAceptar);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Toast.makeText(MainActivity.this, "PASÉ POR AQUÍ", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
